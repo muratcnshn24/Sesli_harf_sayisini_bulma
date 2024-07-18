a170c7a8eb0cf906ea7861bb8118ce5db7c65b97
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 21:32:10 +0300

    Proje dosyası ekle.

---
+++ Sesli_harf_sayisini_bulma/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Sesli_harf_sayisini_bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ifadenin okunmasi
+            string ifade = Console.ReadLine();
+
+            //Sesli Harf
+            int sesliHarf = 0;
+
+            for (int i = 0; i < ifade.Length; i++)
+            {
+                //kontrol
+                if (ifade[i] == 'a' || ifade[i] == 'A' ||
+                    ifade[i] == 'e' || ifade[i] == 'E' ||
+                    ifade[i] == 'ı' || ifade[i] == 'I' ||
+                    ifade[i] == 'i' || ifade[i] == 'İ' ||
+                    ifade[i] == 'u' || ifade[i] == 'U' ||
+                    ifade[i] == 'ü' || ifade[i] == 'Ü' ||
+                    ifade[i] == 'o' || ifade[i] == 'O' ||
+                    ifade[i] == 'ö' || ifade[i] == 'Ö')     // || veya demek.
+
+                    sesliHarf++;
+            }
+
+
+            Console.WriteLine("{0} ifadesinde {1} sesli harf var", ifade, sesliHarf);
+        }
+    }
+}
